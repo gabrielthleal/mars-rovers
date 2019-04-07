c8cb5d54301643d8eb34390819f3e4e02de2afe3
Gabriel Leal <gabrielsilva.leal@hotmail.com>
2019-04-07 10:46:07 -0300

    left and right move is working v2

--- main.rb
+++ main.rb
@@ -4,5 +4,5 @@ require './rover.rb'
 h1 = RoverProblem.new(5, 5)
 h1.run_walle_run(1,2,'N','LMLMLMLMM')
 
-#h2 = RoverProblem.new(5, 5)
-#h2.run_walle_run(3, 3, 'E','MMRMMRMRRM')
+h2 = RoverProblem.new(5, 5)
+h2.run_walle_run(3, 3, 'E','MMRMMRMRRM')
--- rover.rb
+++ rover.rb
@@ -11,12 +11,9 @@ class RoverProblem
   end
   
   def run_walle_run(x, y, cardinal, move)
-    cardinals = %w{N E S W}
-     
+    cardinals_letter = %w{N E S W}
     @y = y
     @x = x
-    left_move = 1
-    right_move = 1
     @actual_point = cardinal
 
     puts "Inicio #{@y} #{@x} #{@actual_point}"
@@ -27,15 +24,12 @@ class RoverProblem
       # cada movimento é o ponto cardinal atual 
       #  DEFINE A DIREÇÃO SEGUNDO OS PONTOS CARDINAIS
       if mov == 'L' 
-        @actual_point = cardinals[-left_move] 
-        left_move += 1
+        @actual_point = cardinals_letter[find_cardinal_index_number - 1] 
       elsif mov == 'R'
-        @actual_point = cardinals[right_move]
-        right_move += 1
+        @actual_point = cardinals_letter[@actual_point == 'W' ? 0 : find_cardinal_index_number + 1]
       end 
       
       if mov == 'M'
-        puts 'ME MOVI'
         @y += 1 if @actual_point == 'N'  
         @x += 1 if @actual_point == 'E'  
         @y -= 1 if @actual_point == 'S'  
@@ -43,11 +37,10 @@ class RoverProblem
       end
       
       @x -= you_shall_not_pass(@x, @limit_x) if @x > @limit_x
-      @y -= you_shall_not_pass(@y, @limit_y) if @y > @limit_y 
-      
-      puts "Meio #{@x} #{@y} #{@actual_point}" 
+      @y -= you_shall_not_pass(@y, @limit_y) if @y > @limit_y       
     end
-    puts "Final #{@x} #{@y} #{@actual_point}"
+
+    "#{@x} #{@y} #{@actual_point}"
   end
 
   def you_shall_not_pass(value, limit)
@@ -55,22 +48,16 @@ class RoverProblem
     return corrected_value.to_i 
   end
 
-  #def find_cardinal_index(@actual_point)
-  #  case @actual_point
-  #  when 'N'
-  #    @left = 0 
-  #    @right = 0
-  #  when 'E'
-  #    @left = 1
-  #    @right = 1
-  #  when 'S'
-  #    @left = 2
-  #    @right = 2
-  #  when 'W'
-  #    @left = 3 
-  #    @right = 3
-  #  else
-  #    
-  #  end
-  #end 
+  def find_cardinal_index_number
+    case @actual_point
+    when 'N'
+      0
+    when 'E'
+      1
+    when 'S'
+      2
+    when 'W'
+      3    
+    end
+  end 
 end
