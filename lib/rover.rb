34c8bc19b5550d839693fa9fbb9ca54dcf435a52
Gabriel Leal <gabrielsilva.leal@hotmail.com>
2019-04-08 19:44:32 +0000

    some corrections on rover class and rspec file

--- lib/rover.rb
+++ lib/rover.rb
@@ -1,59 +1,61 @@
 class RoverProblem
-  CARDINAL_LETTERS = %w{N E S W}.freeze
-  
+  CARDINAL_LETTERS = %w[N E S W].freeze
+
   def initialize(x, y)
     @limit_x = x
     @limit_y = y
   end
-  
-  def run_walle_run(x, y, cardinal, action)
+  # retorno do problema
+  def run_walle_run(x, y, cardinal_position, action)
     @y = y
     @x = x
-    @actual_cardinal_letter = cardinal
-    arr_action = action.split('') 
+    @cardinal_position = cardinal_position
 
-    arr_action.each do |movement|
-     which_side(movement)
-     move_foward(movement)
-     you_shall_not_pass if @x > @limit_x || @y > @limit_y  
+    action.each_char do |mov|
+      looking_at = which_side(mov) 
+      @cardinal_position = actual_cardinal_position(looking_at) 
+      move_foward(mov, @cardinal_position)
+
+      you_shall_not_pass if @x > @limit_x || @y > @limit_y  
+      puts " meio #{@x} #{@y} #{@cardinal_position}" 
     end
-    puts "#{@x} #{@y} #{@actual_cardinal_letter}" 
+    puts "#{@x} #{@y} #{@cardinal_position}" 
   end
 
   private
-
+#retorno da letra cardinal
   def which_side(side)
+    raise 'invalid value' if !['L', 'R', 'M'].include?(side.to_s.upcase)
     if side == 'L' 
-      @actual_cardinal_letter = CARDINAL_LETTERS[find_cardinal_index - 1] 
+      CARDINAL_LETTERS[find_cardinal_index - 1] 
     elsif side == 'R'
-      @actual_cardinal_letter = CARDINAL_LETTERS[@actual_cardinal_letter == 'W' ? 0 : find_cardinal_index + 1]
+      CARDINAL_LETTERS[@cardinal_position == 'W' ? 0 : find_cardinal_index + 1]
     end 
   end
 
-  def move_foward(step)
-    if step == 'M'
-      @y += 1 if @actual_cardinal_letter == 'N'  
-      @x += 1 if @actual_cardinal_letter == 'E'  
-      @y -= 1 if @actual_cardinal_letter == 'S'  
-      @x -= 1 if @actual_cardinal_letter == 'W'
+  def actual_cardinal_position(actual_position) 
+    if actual_position.nil?
+      @cardinal_position
+    else
+      actual_position
     end
   end
 
+  def move_foward(step, looking_at)
+    if step == 'M'
+      @y += 1 if looking_at == 'N'  
+      @x += 1 if looking_at == 'E'  
+      @y -= 1 if looking_at == 'S'  
+      @x -= 1 if looking_at == 'W'
+    end
+  end
+# retorno do x ou y limitado
   def you_shall_not_pass
     @x -= (@x - @limit_x)
     @y -= (@y - @limit_y)
   end
-
+# retorna index
   def find_cardinal_index
-    case @actual_cardinal_letter
-      when 'N'
-        0
-      when 'E'
-        1
-      when 'S'
-        2
-      when 'W'
-        3    
-    end
+    CARDINAL_LETTERS.index(@cardinal_position)
   end 
 end
--- spec/lib/rover_spec.rb
+++ spec/lib/rover_spec.rb
@@ -2,19 +2,41 @@ require 'spec_helper'
 require 'rover'
  
 describe RoverProblem do
-  it 'has a valid limit?' do
-    rover = RoverProblem.new(5,5)
-    expect(rover.limit_x).to be <= 5
-    expect(rover.limit_y).to be <= 5
-  end
-  it 'Sides is L or R?' do
-    rover = RoverProblem.new(5,5)
-    @coordenades = rover.run_walle_run(1,2,'N','LMLMLMLMM')
-    expect(coordenades).to equal('L')
-  end
+  # let(:rover) {RoverProblem.new(5,5)}
+    it 'is x more than valid?' do
+      rover = RoverProblem.new(5,5)
+      expect(rover.limit_x).to be > 0
+    end
+    it 'is y more than valid?' do
+      rover = RoverProblem.new(5,5)
+      expect(rover.limit_y).to be > 0
+    end
+
+    it 'first rover deployed and movimentation is working!' do
+      rover = RoverProblem.new(5,5)
+      expect(rover.run_walle_run(1,2,'N','LMLMLMLMM')).to eq('1 2 N')
+    end
+
+    it 'second rover deployed and movimentation is working!' do
+      rover = RoverProblem.new(5,5)
+      expect(rover.run_walle_run(3, 3, 'E','MMRMMRMRRM')).to eq('5 1 E')
+    end
+
+    # it 'is a valid side?' do
+    #   rover = RoverProblem.new(5,5)
+    #   rover.run_walle_run(3,2,'E','OPMPMEM')
+    #   expect(rover).to eq('L')
+    # end
+end
+
+  # it 'Sides is L or R?' do
+  #   rover = RoverProblem.new(5,5)
+  #   @coordenades = rover.run_walle_run(1,2,'N','LMLMLMLMM')
+  #   expect(coordenades).to equal('L')
+  # end
   #it 'Cardinals points' do
   #  expect(coordenades.@actual_cardinal_letter).to equal('N')
   #end
   #it 'cardinal point is right?'
   #it 'action of moviments is right?'
-end
+# end
